bfee691a553bb405aa3f96dd78ef3f02ccbb7b8c
kanji14134 <kanji14134@gmail.com>
2016-01-14 16:00:43 +0900

    sp

--- model_master.R
+++ model_master.R
@@ -3,6 +3,7 @@ source("R2WBwrapper.R")
 load("C:/Users/gisadmin/Dropbox/学会発表/2016_生態学会/bayes_data/d5.Rdata")
 
 setwd("F:/test/testmodel")
+sp?
 
 ########################################model1
 m1<-"model{
